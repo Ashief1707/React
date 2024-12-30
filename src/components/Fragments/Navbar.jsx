e24d13903e76eca62eca74e330a0eecd4cb8da0b
Ashief1707 <muhammadashief17@gmail.com>
2024-12-30 14:32:33 +0700

    persiapan UAS

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -5,6 +5,7 @@ import { useContext } from 'react';
 import { ThemeContext } from '../../context/themeContext';
 import { AuthContext } from '../../context/authContext';
 import axios from "axios";
+import { NotifContext } from '../../context/notifContext';
 
 export const Navbar = () => {
 
@@ -18,17 +19,23 @@ export const Navbar = () => {
   
   const {theme, setTheme} = useContext(ThemeContext);
   const { setIsLoggedIn, setName, name } = useContext(AuthContext);
+  const { setMsg, setOpen, setIsLoading } = useContext(NotifContext);
   const navigate = useNavigate();
 
   const refreshToken = localStorage.getItem("refreshToken");
 
 const Logout = async () => {
+  setIsLoading(true);
     try {
       await axios.get("https://jwt-auth-eight-neon.vercel.app/logout", {
         headers: {
           Authorization: `Bearer ${refreshToken}`,
         },
       });
+
+      setIsLoading(false);
+      setOpen(true);
+      setMsg({ severity: "success", desc:"Logout Success"})
 
       setIsLoggedIn(false);
       setName("");
@@ -43,6 +50,13 @@ const Logout = async () => {
         setMsg({ severity: "error", desc: error.response.data.msg });
       }
     }
+    setIsLoggedIn(false);
+    setName("");
+    setIsLoading(false);
+
+    localStorage.removeItem("refreshToken");
+    navigate("/login");
+
   };
 
   const menus = [
@@ -103,8 +117,8 @@ const Logout = async () => {
             to={menu.link}
             className={({ isActive }) => 
               isActive
-              ? "flex bg-primary text-white font-bold px-4 py-3 rounded-md"
-              : "flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md"
+              ? "flex bg-primary text-white font-bold px-4 py-3 rounded-md zoom-in"
+              : "flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md zoom-in"
             }
           >
             <div className="mx-auto sm:mx-0">{menu.icon}</div>
@@ -117,13 +131,13 @@ const Logout = async () => {
         {themes.map((t) => (
           <div
             key={t.name}
-            className={`${t.bgcolor} md:w-6 h-6 rounded-md cursor-pointer mb-2`}
+            className={`${t.bgcolor} md:w-6 h-6 rounded-md cursor-pointer mb-2 zoom-in`}
             onClick={() => setTheme(t)}
           ></div>
         ))}
       </div>
       <div className="sticky bottom-12">
-        <NavLink onClick={Logout} className="flex bg-special-bg3 px-4 py-3 rounded-md">
+        <NavLink onClick={Logout} className="flex bg-special-bg3 px-4 py-3 rounded-md zoom-in">
           <div className="mx-auto sm:mx-0 text-primary">
             <Icon.Logout />
           </div>
